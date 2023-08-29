723a4447346be681d41f3d38217eace801db06e1
Kleber Kelf <kleberkelf@gmail.com>
2023-08-28 22:11:16 -0400

    Fix in images path

--- src/NovoBoletoPHP/BoletoFactory.php
+++ src/NovoBoletoPHP/BoletoFactory.php
@@ -80,7 +80,6 @@ class BoletoFactory {
 
     public function toBase64($path)
     {
-        $path = ($path[0] === '/') ? substr($path, 1) : $path;
         $type = pathinfo($path, PATHINFO_EXTENSION);
         $data = file_get_contents($path);
         $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
