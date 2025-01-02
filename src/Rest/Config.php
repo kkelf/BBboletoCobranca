d7079a85c5f6907d5e1b7219e4bad16344b60cd8
kkelf <kleberkelf@gmail.com>
2025-01-02 14:37:09 -0400

    fix: endpoint de homologacao

--- src/Rest/Config.php
+++ src/Rest/Config.php
@@ -4,7 +4,7 @@ namespace BBboletoCobranca\Rest;
 
 class Config
 {
-	const ENDPOINT_HM = 'https://api.sandbox.bb.com.br/cobrancas/v2';
+	const ENDPOINT_HM = 'https://api.hm.bb.com.br/cobrancas/v2';
 
     const ENDPOINT_PRODUCTION = 'https://api.bb.com.br/cobrancas/v2';
 }
